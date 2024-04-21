9687091c9f78bacfdb9c15bcb6ab97725ebf17ed
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-04-21 13:38:53 +0300

    Initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+function Button({ buttonType, children, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`btn ${buttonType === 'secondary' ? 'btn--secondary' : ''}`}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import Logo from './Logo';
+import Counter from './Counter';
+import { useItemsStore } from '../stores/itemsStore';
+
+function Header() {
+  const items = useItemsStore((state) => state.items);
+  return (
+    <header>
+      <Logo />
+      <Counter
+        numberOfItemsPacked={items.filter((item) => item.packed).length}
+        totalNumberOfItems={items.length}
+      />
+    </header>
+  );
+}
+
+export default Header;
